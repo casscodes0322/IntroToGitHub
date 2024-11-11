b575773a75b7fc739cd15a6d85adf960830e24f2
casscodes0322 <cashoward2006@gmail.com>
2024-11-11 14:57:01 -0700

    second commit

---
+++ test.cpp.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> //opens all libraries
+using namespace std;
+
+int main(){
+    
+    string fruit;
+    
+    cout << "Apples or oranges?" << endl;
+    cin >> fruit;
+
+    cout << "Cool!" << endl;
+
+    return 0;
+}
